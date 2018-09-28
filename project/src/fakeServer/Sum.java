42c06d5dc0dbe8b62b0cf4df0e3d19d756dea0d0
winterhart <m.user.work@gmail.com>
2018-09-28 09:28:37 -0400

    Removal of the fakeServer pacakge, Transfert fake method client to the client in frontEnd package

--- project/src/fakeServer/Sum.java
+++
@@ -1,8 +0,0 @@
-package fakeServer;
-
-import java.rmi.*;
-
-public interface Sum extends Remote {
-	
-	public int add(int x, int y) throws RemoteException;
-}
--- project/src/frontEnd/Client.java
+++ project/src/frontEnd/Client.java
@@ -1,10 +1,27 @@
 package frontEnd;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import shared.IHRActions;
+
 public class Client {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		Registry reg = LocateRegistry.getRegistry(2462); 
+		IHRActions sum = (IHRActions) reg.lookup("CA");
+		sum.sayHello();
+		System.out.println("Client is off... ");
+		
+		Registry rege = LocateRegistry.getRegistry(2796); 
+		IHRActions sume = (IHRActions) rege.lookup("US");
+		sume.sayHello();
+		System.out.println("Client is off... ");
+		
+		Registry regr = LocateRegistry.getRegistry(8352); 
+		IHRActions sumr = (IHRActions) regr.lookup("UK");
+		sumr.sayHello();
+		System.out.println("Client is off... ");
 	}
 
 }
